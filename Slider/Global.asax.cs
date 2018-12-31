91342a7633763871aac2e3dea1e28f168f82eb2b
Unknown <oktawianlasek@gmail.com>
2018-12-31 21:52:47 +0100

    ok, przezyl update nugetow

--- Slider/Global.asax.cs
+++ Slider/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using VoteService;
+
 
 namespace Slider
 {
--- Slider/Models/IdentityModels.cs
+++ Slider/Models/IdentityModels.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace Slider.Models
 {
@@ -32,3 +32,5 @@ namespace Slider.Models
             return new ApplicationDbContext();
 
         }
+    }
+}
