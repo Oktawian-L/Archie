2e121b8a7ece9060afeefa3bbab8ce22ad43296b
Unknown <oktawianlasek@gmail.com>
2018-12-31 22:30:56 +0100

    nuget sie posypal

--- Slider/App_Start/Startup.Auth.cs
+++ Slider/App_Start/Startup.Auth.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
 using Slider.Models;
+using Slider.Providers;
 
 namespace Slider.App_Start
 {
