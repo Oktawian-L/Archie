d4a935ab9645d6c10706f5aa782e769b19f60a09
Unknown <oktawianlasek@gmail.com>
2018-12-31 21:38:41 +0100

    still wokring

--- Slider/App_Start/FilterConfig.cs
+++ Slider/App_Start/FilterConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 
-namespace Slider
+namespace VoteService
 {
     public class FilterConfig
     {
--- Slider/App_Start/RouteConfig.cs
+++ Slider/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Slider
+namespace VoteService
 {
     public class RouteConfig
     {
