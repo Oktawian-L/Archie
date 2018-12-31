8cd3167bde2a858c9f01ec42e373bfcb8af572f5
Unknown <oktawianlasek@gmail.com>
2018-12-31 21:02:01 +0100

    archie+slider

--- Slider.Tests/Properties/AssemblyInfo.cs
+++ Slider.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Slider")]
+[assembly: AssemblyTitle("Slider.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Slider")]
+[assembly: AssemblyProduct("Slider.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ebb629c2-c030-4d8b-8bae-a6839d824c30")]
+[assembly: Guid("abaff1b3-5b13-4bf0-aeea-fdd5f10323d2")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Suwak_vote/README.md
+++
@@ -1,8 +0,0 @@
-Porting of the Python "vote" app is currently work in progress, use curl instead against the ASP.NET webservice:
-
-i.e. 
-
-Submit a vote:
-```
-curl -X POST -d '{}' http://serviceIP/voting/v/1/submit/{voteid}/a
-```
