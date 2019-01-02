938cd8b758df65136e34752a641fde3ed3ceecf2
Unknown <oktawianlasek@gmail.com>
2019-01-02 22:12:24 +0100

    probuije front end docker compose umiescic

--- Suwak_result/server.js
+++ Suwak_result/server.js
@@ -1,71 +1,16 @@
-var express = require('express'),
-    async = require('async'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    app = express(),
-    server = require('http').Server(app),
-    io = require('socket.io')(server),
-    request = require('request');
+'use strict';
 
-io.set('transports', ['polling']);
+const express = require('express');
 
-var port = process.env.PORT || 4000;
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
-io.sockets.on('connection', function (socket) {
-
-  socket.emit('message', { text : 'Welcome!' });
-
-  socket.on('subscribe', function (data) {
-    socket.join(data.channel);
-  });
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
 });
 
-function getVotes() {
-   console.log("Getting votes");
-
-   var options = {
-      uri: process.env.VOTE_SERVICE_URL,
-      json: true
-    };
-
-    request.get(options, function(err, res, body) {
-      if (err) {
-        console.error("Waiting for db");
-        console.log(body);  
-      }else {
-        var votes = collectVotesFromResult(body);
-        io.sockets.emit("scores", JSON.stringify(votes));
-      }
-      setTimeout(function() {getVotes() }, 1000);
-    });
-}
-
-function collectVotesFromResult(result) {
-  var votes = {a: result.VoteA, b: result.VoteB};
-  return votes;
-}
-
-app.use(cookieParser());
-app.use(bodyParser());
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  next();
-});
-
-app.use(express.static(__dirname + '/views'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname + '/views/index.html'));
-});
-
-server.listen(port, function () {
-  var port = server.address().port;
-  console.log('App running on port ' + port);
-});
-
-
-getVotes();
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
