58f63eb2c1e316a1bc85747495c8f3cd6ec04982
Unknown <oktawianlasek@gmail.com>
2019-01-02 19:44:52 +0100

    dodaje controller dla backendu

---
+++ GoldAPI/Controllers/CatalogController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+
+namespace GoldAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
